7840c0688e81dc1a7d37a38925169a01511065e0
DaeHan <43459030+daehanchoi-dev@users.noreply.github.com>
2020-03-17 14:50:53 +0900

    Add files via upload

---
+++ 2231.py
@@ -0,0 +1,21 @@
+import sys
+num = sys.stdin.readline()
+
+gen_num = int(num) - (len(num)*9)
+
+if gen_num < 0:
+    gen_num = int(num)
+
+while gen_num < int(num):
+    each_num_sum = sum([int(i) for i in str(gen_num)])
+    result = each_num_sum + gen_num
+
+    if result == int(num):
+        print(gen_num)
+        break
+    else:
+        gen_num +=1
+
+if gen_num == int(num):
+    print(0)
+
---
+++ 7568.py
@@ -0,0 +1,19 @@
+N = int(input())
+people = []
+
+
+for _ in range(N):
+    w, h = map(int, input().split())
+    people.append((w,h))
+
+for a in people:
+    rank =1
+
+    for b in people:
+        if(a[0] != b[0]) and (a[1] != b[1]):
+            if(a[0] < b[0]) and (a[1] < b[1]):
+                rank += 1
+
+    print(rank)
+
+
