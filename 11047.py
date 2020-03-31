98cf0904c8b44188ce4b9ca1c54ef47a04440b5f
DaeHan <43459030+daehanchoi-dev@users.noreply.github.com>
2020-03-31 18:45:02 +0900

    Add files via upload

---
+++ 11047.py
@@ -0,0 +1,19 @@
+# 동전 (그리디알고리즘)
+N, K = map(int, input().split())
+coins = [int(input()) for _ in range(N)]
+
+cnt = 0
+
+for i in range(1, N + 1):
+    coin = coins[-i]
+    print('coin = ', coin)
+    while True:
+        if K >= coin:
+            K -= coin
+            cnt += 1
+            if K == 0:
+                break
+        else:
+            break
+
+print(cnt)
