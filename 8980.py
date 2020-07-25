b07574d502511b19b23f502634fd2d884701c8ab
DaeHan <43459030+daehanchoi-dev@users.noreply.github.com>
2020-07-25 23:05:20 +0900

    Add files via upload

---
+++ 8980.py
@@ -0,0 +1,11 @@
+N, C = map(int, input().split())
+M = int(input())
+#Box = [list(input()) for _ in range(M)]
+
+
+for i in range(M):
+    Box = list(input())
+    if Box[0] == 1:
+        print('hi')
+
+
