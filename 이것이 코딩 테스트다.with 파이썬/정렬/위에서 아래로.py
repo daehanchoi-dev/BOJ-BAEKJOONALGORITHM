0bae9311eb26d8a3d4e3571e01c41871237f7852
DaeHan <43459030+daehanchoi-dev@users.noreply.github.com>
2020-08-16 12:49:38 +0900

    Update 위에서 아래로.py

--- 이것이 코딩 테스트다.with 파이썬/정렬/위에서 아래로.py
+++ 이것이 코딩 테스트다.with 파이썬/정렬/위에서 아래로.py
@@ -1,12 +1,24 @@
-# 선택정렬
-array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+"""
+하나의 수열에는 다양한 수가 존재한다. 이러한 수는 크기에 상관없이 나열되어 있다.
+이 수를 큰 수 부터 작은 수의 순서로 정렬해야 한다. 수열을 내림차순으로 정렬하는 프로그램을 만드시오.
 
-for i in range(len(array)):
-    min_index = i
-    for j in range(i+1, len(array)):
-        if array[min_index] > array[j]:
-            min_index = j
-    array[i], array[min_index] = array[min_index], array[i] # 스와프
-    print(array[i], array[min_index])
+입력조건 :
+- 첫째 줄에 수열에 속해 있는 수의 개수 N이 주어진다.
+- 둘째 줄부터 N+1번째 줄까지 N개의 수가 입력된다. 수의 범위는 1이상 100,000 이하의 자연수이다.
 
-print(array)
+출력조건 :
+- 입력으로 주어진 수열이 내림차순으로 정렬된 결과를 공백으로 구분하여 출력한다. 동일한 수의 순서는 자유롭게 출력해도 괜찮다.
+"""
+# 정수 N 입력받기
+n = int(input())
+
+# N개의 정수를 입력받아 리스트에 저장
+array = []
+for i in range(n):
+    array.append(int(input()))
+
+# array 리스트를 내림차순으로 정렬
+array.sort(reverse=True)
+
+for i in array:
+    print(i, end=' ')
