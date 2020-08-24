4a097ceea1643becffe48c33d2e37d26cb3ea5af
DaeHan <43459030+daehanchoi-dev@users.noreply.github.com>
2020-08-24 15:02:48 +0900

    Update 1로 만들기.py

--- 이것이 코딩 테스트다.with 파이썬/다이나믹 프로그래밍/1로 만들기.py
+++ 이것이 코딩 테스트다.with 파이썬/다이나믹 프로그래밍/1로 만들기.py
@@ -1,11 +1,13 @@
-d = [0] * 1000
+x= int(input())
 
-def fibo(x):
-    if x == 1 or x == 2:
-        return 1
-    if d[x] != 0:
-        return d[x]
-    d[x] = fibo(x-1) + fibo(x-2)
-    return d[x]
+d = [0] * 30001
 
-print(fobi(99))
+for i in range(2, x+1):
+    d[i] = d[i-1] + 1
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i//2] + 1)
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i//3] + 1)
+    if i % 5 == 0:
+        d[i] == min(d[i], d[i//5] + 1)
+print(d[x])
